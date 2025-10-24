4bf479c4c43f2274167775d699def9458bd9baa6
DmytroPylypenko <pilya.dima@gmail.com>
2025-10-24 17:17:32 +0300

    feat: Add DbSets to ApplicationDbContext

--- TaskFlow.Api/Data/ApplicationDbContext.cs
+++ TaskFlow.Api/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using TaskFlow.Api.Models;
+using Task = TaskFlow.Api.Models.Task;
 
 namespace TaskFlow.Api.Data;
 
@@ -6,6 +8,10 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     {
-        
     }
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Board> Boards { get; set; }
+    public DbSet<Column> Columns { get; set; }
+    public DbSet<Task> Tasks { get; set; }
 }
