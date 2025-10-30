259bba2fc3102c0ec0634c6e20e9b50269617632
DmytroPylypenko <pilya.dima@gmail.com>
2025-10-30 20:38:55 +0200

    feat(db): add position column to tasks table
    Creates and applies a new database migration to add a 'Position' column, enabling task reordering.

--- TaskFlow.Api/Migrations/20251030183733_AddPositionToTasks.Designer.cs
+++ TaskFlow.Api/Migrations/20251030183733_AddPositionToTasks.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskFlow.Api.Data;
 
@@ -10,9 +11,11 @@ using TaskFlow.Api.Data;
 namespace TaskFlow.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251030183733_AddPositionToTasks")]
+    partial class AddPositionToTasks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- TaskFlow.Api/Models/Task.cs
+++ TaskFlow.Api/Models/Task.cs
@@ -14,6 +14,8 @@ public class Task
     [MaxLength(500)]
     public string? Description { get; set; }
 
+    public int Position { get; set; }
+    
     public int ColumnId { get; set; }
     
     [JsonIgnore]
